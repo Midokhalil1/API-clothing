b23be920ce14817c4f3551d85e7b0a09edd80d2e
Midokhalil1 <midokhalil22@gmail.com>
2022-10-21 13:36:54 -0400

    API

---
+++ functions/secrets.js
@@ -0,0 +1,14 @@
+export default {
+  type: "service_account",
+  project_id: "mido-brad-api",
+  private_key_id: "a03e060aba67f0e6dae1ebd0ceb4e7feff8face6",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCWWInE2MEBM1Vh\npvlPq6R7ZXfdI12MKAxzbdWmjqvxQ8FaT0nlpMc4bQUQn3ucvZg64+hZmGmbM34+\nLFQW+Fvech/geP4YfzkobxyUIzWtS/7fg8vZQxk2dPnKco27fQpYEG2r2Fyb8H8W\nGcxx24FpnI7W0c8RqfwzWdTrtlEPTRDxbdqYTFIbhFGcvC6nLXsEsmU1O+2GUUX5\nReFoIMZGCOaDopdrXGs0miTE9mtLEWCs7j9cj4LrlJvdpaomlpj4npK9jNus7epz\nJOXZ8aadtYNMBbmN+EEJJ3EJrCL2fokISKhX5ZeGKf1zgvUt0OoyjPB9yxWvKeWO\nVqijCbfzAgMBAAECggEAKlY+XX9IQmVJOtmBRU0pYVI+gbhOxIQWlX6tdlyg0Kzq\nytqfDZrsOtiAX6D5dzbwokzBhlFkE79ZYnqrJ1jCzg5HKbgIna9d0iQP4StVFzra\nR+jeQ40gkXQ5TygY/14Dxq7Bo22nDoaX0wpqxvkwJSRgGOA2Nlwu1+sJCWNqyirU\nAvq8sihHRSCUneOrKXQ9+b5i/8ciHY05nnRFbshf5+/4Zqn9sZfARuGV9DRbzdap\nykFNe9ZCAc8JY0hwdrdZGwS7wElrGPZp6B55dupdfxw+OdyQBGctFvLfenJVnRwB\nOVzWpjJ3UK8W77++mXDeDjcqHJGJ+Jh6/y95su4ZoQKBgQDHzk41JecgssatXXKw\nnDCXLIu+qcMqtz9VSL4cxYU6xZqEU3/pr/PNwdYHYltIhvK7p9+/1Hy4VhDmdllW\nkEx51ipMR861ppoux8i2yXkcot709zgNgfdSsOIMHT05wvxWSltEeZBbhAAI+6zh\nlqDTQa4VQj7D4SrStXypRh8Z4wKBgQDAoTEoHlySpK6UopqHh4VzI1DoLs7Htbxg\nv6biNWnHLEKZo0creEclDE//gjqW6Pvfq4AcKt+0WNNiXSH3Wk+6a/1bfSzzCpep\n/VX5o7THDL/7dSCK5PBHPvvTy29zexdazEUH1H8T7m7RmOH8xO4KRmZOzOnK6mKf\nKakr0NSGsQKBgB5Wjft18N0/J/mk6cg1Pw+FPQbLQg4XWu95Jxy5RRyCao7TGuYB\nyHQijXv7jD679tlViRzYh8N5lgFYUQJa0LHjOfLxKErerCSUQz614sTdWhs5+nHa\nnRIaN4h9XBGMMMJtceQ4zRmINnUmub5bH7pUC7nAoEUfyF/MHMybEBffAoGBAJEU\n1sEwnOiVwCeSkHCjUY92eell17fy2P7FWhvlvtWUMFUm9ON4o5zzuQzoONiQgaPk\nt4h/gh2DCTJ8kqiKDKKL0zalAMXMpI4TkD2FXxLMN4xcmzYBWDdQ6LqL7lU70EtC\nrntHQK7yVeTnEM4gRGjYvRV8oFmETuMxrYiisG8BAoGAY+uMXQ0PPvFoO1is4dBo\niuoNGYRivIpuicV16WRh3ijJTtilkKt4EU2Oe6GXtQnOyjYxvRUhyJTW6qsHXrVx\n6d+v7LBrZNUuY+xJItH5bEa2s7/q7n5MUfHxQNLrW/EmBEPHokoW5AOHoUQLexpH\nZSWBdXWuKQ7w7M60+o4fU+Q=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-cwhd0@mido-brad-api.iam.gserviceaccount.com",
+  client_id: "108047547472927373200",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cwhd0%40mido-brad-api.iam.gserviceaccount.com",
+};
